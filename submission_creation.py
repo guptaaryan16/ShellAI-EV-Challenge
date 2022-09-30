8388021991135562468ca5d17e5e0836f3dfd245
Sanidhya Singh <sanidhyas3s@gmail.com>
2022-10-01 00:37:33 +0530

    Fixed order and missing supply_point_index value

--- submission_creation.py
+++ submission_creation.py
@@ -7,16 +7,16 @@ warnings.filterwarnings("ignore")
 
 submit = pd.DataFrame(columns=["year","data_type","demand_point_index","supply_point_index","value"])
 for i in range(100):
-    submit.loc[len(submit.index)]=[2019,"SCS","","",SCS_2019[i]]
+    submit.loc[len(submit.index)]=[2019,"SCS","",i,SCS_2019[i]]
 for i in range(100):
-    submit.loc[len(submit.index)]=[2019,"FCS","","",FCS_2019[i]]  
-for i in range(100):
-    submit.loc[len(submit.index)]=[2020,"SCS","","",SCS_2020[i]]
-for i in range(100):
-    submit.loc[len(submit.index)]=[2020,"FCS","","",FCS_2020[i]]  
+    submit.loc[len(submit.index)]=[2019,"FCS","",i,FCS_2019[i]]  
 for i in range(4096):
     for j in range(100):
         submit.loc[len(submit.index)]=[2019,"DS",i,j,dsmatrix2019[i][j]]
+for i in range(100):
+    submit.loc[len(submit.index)]=[2020,"SCS","",i,SCS_2020[i]]
+for i in range(100):
+    submit.loc[len(submit.index)]=[2020,"FCS","",i,FCS_2020[i]]  
 for i in range(4096):
     for j in range(100):
         submit.loc[len(submit.index)]=[2020,"DS",i,j,dsmatrix2020[i][j]]
